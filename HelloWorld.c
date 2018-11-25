f1e2d2094b7bad89f0a7ac708d03801af84f5d89
johannesgrossmusik <johannes@grossmusik.de>
2018-11-25 13:05:19 +0100

    Improved the HelloWorld to support more parameters.

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 void main(int argc, char* argv[]){
+
+// Used with one argument/parameter
+if(2 == argc)
+	printf("%s: 'Hello %s'", argv[0], argv[1]);
+
+// Used with two arguments/parameters
+else if(argc > 2)
+	printf("%s: 'Hello %s %s'", argv[0], argv[1], argv[2]);
+
+// Used without any arguments/parameters
+else
 	printf("%s: 'Hello World'", argv[0]);
 }
